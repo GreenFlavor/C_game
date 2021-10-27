79f5a211d84b83996e2de00e7f095642e89fd488
D.H.LEE <D.H.LEE@DESKTOP-EHCMV8I>
2021-10-27 11:03:16 +0900

    다차원배열

--- C_game/pointer.c
+++ C_game/pointer.c
@@ -12,7 +12,7 @@ void printfFishes();
 void decreaseWater(long elapsedTime);
 int checkFishAlive();
 
-int main(void)
+int mainfish(void)
 {
 	long startTime = 0; // 게임 시작 시간
 	long totalElapsedTime = 0; // 총 경과 시간
